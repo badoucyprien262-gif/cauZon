2535f14053d9c18d20182e532d5f236715b1d2a5
cyprien Badou <badoucyprien262@gmail.com>
2026-09-01 16:36:32 +0000

    alignement stack technique site internet

---
+++ Dossier du site internet/src/types/index.ts
@@ -0,0 +1,33 @@
+﻿export interface DocumentSummary {
+  id: string;
+  titre: string;
+  categorie: string;
+  nombre_pages: number;
+  est_certifie: boolean;
+  prix: number;
+}
+
+export interface PricingPlan {
+  id: string;
+  title: string;
+  badge?: string;
+  price: string;
+  period?: string;
+  description: string;
+  features: string[];
+  cta: string;
+  highlighted?: boolean;
+  tag?: string;
+}
+
+export interface FAQItem {
+  question: string;
+  answer: string;
+}
+
+export interface FeatureItem {
+  icon: string;
+  title: string;
+  description: string;
+  tag: string;
+}
